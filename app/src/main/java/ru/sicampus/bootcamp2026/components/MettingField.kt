9824248d7511aa0d2a0dfa84774093786455b7a2
xakquti <166896320+xakquti@users.noreply.github.com>
2026-01-30 22:51:59 +0800

    Расписание

---
+++ app/src/main/java/ru/sicampus/bootcamp2026/components/MettingField.kt
@@ -0,0 +1,64 @@
+package ru.sicampus.bootcamp2026.components
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ru.sicampus.bootcamp2026.R
+import ru.sicampus.bootcamp2026.screens.ScheduleScreen
+import ru.sicampus.bootcamp2026.ui.theme.Black
+import ru.sicampus.bootcamp2026.ui.theme.Blue
+import ru.sicampus.bootcamp2026.ui.theme.DarkGrey
+import ru.sicampus.bootcamp2026.ui.theme.LightGrey
+
+@Composable
+fun MeetingField(
+    label: String,
+    time: String
+) {
+
+    Card(modifier = Modifier.fillMaxWidth().height(100.dp)
+        .clip(RoundedCornerShape(20.dp))) {
+        Row(modifier = Modifier.fillMaxSize().padding(horizontal = 10.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+            Icon(painter = painterResource(R.drawable.meeting)
+                , contentDescription = "Обсуждение")
+            Text(label, fontSize = 18.sp, fontFamily = FontFamily(androidx.
+            compose.ui.text.font.Font(R.font.montserrat_bold)),
+                color = Black,
+                modifier = Modifier
+                .padding(horizontal = 16.dp))
+            Text(time, fontSize = 12.sp, fontFamily = FontFamily(androidx.
+            compose.ui.text.font.Font(R.font.montserrat_regular)),
+                color = DarkGrey,
+                modifier = Modifier
+                    .padding(start=24.dp))
+
+        }
+    }
+
+}
--- app/src/main/java/ru/sicampus/bootcamp2026/screens/ScheduleScreen.kt
+++ app/src/main/java/ru/sicampus/bootcamp2026/screens/ScheduleScreen.kt
@@ -1,12 +1,129 @@
 package ru.sicampus.bootcamp2026.screens
 
+import android.R
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.dropShadow
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ru.sicampus.bootcamp2026.components.MeetingField
+import ru.sicampus.bootcamp2026.ui.theme.Black
+import ru.sicampus.bootcamp2026.ui.theme.Blue
+import ru.sicampus.bootcamp2026.ui.theme.White
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun ScheduleScreen() {
-    Text("Расписание", modifier = Modifier.fillMaxSize().wrapContentHeight())
+    val scrollState = rememberScrollState()
+
+
+    Column(modifier = Modifier.fillMaxSize().background(White)) {
+        Card(
+            modifier = Modifier.fillMaxWidth()
+                .height(389.dp),
+            colors = CardDefaults.cardColors(contentColor = White),
+            shape = RoundedCornerShape(bottomStart = 30.dp,
+                bottomEnd = 30.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+        )
+        {
+            Text(text = "Jan, 2026",
+                fontSize = 20.sp,
+                color = Black,
+                fontFamily = FontFamily(androidx.
+                compose.ui.text.font.Font(ru.sicampus.bootcamp2026.R.font.montserrat_bold)),
+                modifier = Modifier.padding(horizontal = 24.dp)
+                    .padding(top=60.dp)
+            )
+            Row (modifier = Modifier.fillMaxWidth().padding(horizontal = 24.dp).padding(top=47.dp)){
+                Column(modifier = Modifier
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(Blue)
+                    .height(83.dp)
+                    .width(74.dp)
+                    .clip(RoundedCornerShape(30.dp))) {
+                    Text(text = "28",
+                        fontSize = 20.sp,
+                        color = White,
+                        fontFamily = FontFamily(androidx.
+                        compose.ui.text.font.Font(ru.sicampus.bootcamp2026.R.font.montserrat_bold)),
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .align(Alignment.CenterHorizontally) )
+                    Text(text = "Ср",
+                        fontSize = 20.sp,
+                        color = White,
+                        fontFamily = FontFamily(androidx.
+                        compose.ui.text.font.Font(ru.sicampus.bootcamp2026.R.font.montserrat_regular)),
+                        modifier = Modifier
+                            .padding(2.dp)
+                            .align(Alignment.CenterHorizontally) )
+                    Spacer(modifier = Modifier.width(10.dp))
+
+
+
+                }
+            }
+        }
+
+        Text(text = "Встречи",
+            fontSize = 20.sp,
+            color = Black,
+            fontFamily = FontFamily(androidx.
+            compose.ui.text.font.Font(ru.sicampus.bootcamp2026.R.font.montserrat_bold)),
+            modifier = Modifier.padding(horizontal = 24.dp)
+                .padding(top=30.dp)
+        )
+
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+            .verticalScroll(scrollState)){
+            MeetingField("Обсуждение 1", "18:00")
+            Spacer(modifier = Modifier.height(30.dp))
+            MeetingField("Обсуждение 2", "19:00")
+            Spacer(modifier = Modifier.height(30.dp))
+            MeetingField("Обсуждение 3", "20:00")
+            Spacer(modifier = Modifier.height(30.dp))
+            MeetingField("Обсуждение 4", "21:00")
+            Spacer(modifier = Modifier.height(30.dp))
+        }
+        Spacer(modifier = Modifier.height(150.dp))
+
+    }
+
+}
+
+@Preview
+@Composable
+fun Show5() {
+    ScheduleScreen()
 }
