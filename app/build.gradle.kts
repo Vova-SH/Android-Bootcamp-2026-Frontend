e4ee0369a917f93d6dbee2b406077be3ef9bc878
solovushka56 <75mehanik@gmail.com>
2026-02-01 15:19:20 +0300

    deps: added viewmodel, kror, coil deps, etc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    //base
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -56,4 +56,12 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+//    implementation(libs.androidx.compose.material3) // ?
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.ktor3)
+
+
 }
