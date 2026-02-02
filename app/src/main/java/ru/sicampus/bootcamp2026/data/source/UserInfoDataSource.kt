880b36ac717f3af69f056005045f728744be19d5
ilya <128222865+jiko-star@users.noreply.github.com>
2026-02-02 17:58:19 +0300

    add dto

---
+++ app/src/main/java/ru/sicampus/bootcamp2026/data/source/UserInfoDataSource.kt
@@ -0,0 +1,22 @@
+package ru.sicampus.bootcamp2026.data.source
+
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.http.HttpStatusCode
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import ru.sicampus.bootcamp2026.data.dto.UserDto
+
+class UserInfoDataSource {
+    suspend fun getUser(): Result <List<UserDto>> = withContext(Dispatchers.IO){
+        runCatching {
+            val result = Network.client.get("${Network.HOST}/api/person")
+            if (result.status != HttpStatusCode.OK) {
+                error("Status: ${result.status}")
+            }
+            result.body()
+
+        }
+
+        }
+}
